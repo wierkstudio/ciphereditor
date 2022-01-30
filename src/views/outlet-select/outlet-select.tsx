c67defc24fafd65f4a88949eb65298408fa3c115
Fränz Friederes <fraenz@frieder.es>
2022-01-30 22:39:09 +0100

    Separate OutletSelectView from OutletView

---
+++ src/views/outlet-select/outlet-select.tsx
@@ -0,0 +1,145 @@
+
+import SelectView, { SelectViewElement, SelectViewGroupElement } from 'views/select/select'
+import {
+  addVariableFromControlAction,
+  attachControlToVariableAction,
+  detachControlFromVariableAction
+} from 'slices/blueprint'
+import { ControlNode } from 'slices/blueprint/types/control'
+import { ProgramNode } from 'slices/blueprint/types/program'
+import { getControlVariable, getVariableControl } from 'slices/blueprint/selectors/variable'
+import { getControlVariableOptions } from 'slices/blueprint/selectors/control'
+import { useAppDispatch, useBlueprintSelector } from 'utils/hooks'
+import { useCallback } from 'react'
+
+export default function OutletSelectView(props: {
+  control: ControlNode
+  program: ProgramNode
+}) {
+  const dispatch = useAppDispatch()
+  const { control, program } = props
+  const controlId = control.id
+  const programId = program.id
+
+  const { pushOptions, pullOptions } = useBlueprintSelector(state =>
+    getControlVariableOptions(state, props.control.id, props.program.id))
+
+  const attachedVariable = useBlueprintSelector(state =>
+    getControlVariable(state, props.control.id, props.program.id))
+  const attachedVariableSourceControl = useBlueprintSelector(state =>
+    attachedVariable ? getVariableControl(state, attachedVariable.id) : undefined)
+  const attachedVariableId = attachedVariable?.id
+
+  const onSelectChange = useCallback((event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value
+    const type = value[0]
+    const choiceIndex = parseInt(value.substring(1))
+    let variable
+    switch (type) {
+      case 'u':
+        if (attachedVariableId !== undefined) {
+          // Detach variable
+          dispatch(detachControlFromVariableAction({
+            controlId,
+            variableId: attachedVariableId,
+          }))
+        }
+        break
+      case 's':
+        // Push to a variable
+        variable = pushOptions[choiceIndex]
+        if (variable) {
+          dispatch(attachControlToVariableAction({
+            controlId,
+            variableId: variable.id,
+            push: true,
+          }))
+        }
+        break
+      case 'n':
+        // Push to a new variable
+        dispatch(addVariableFromControlAction({
+          controlId,
+          programId,
+        }))
+        break
+      case 'l':
+        // Pull from a variable
+        variable = pullOptions[choiceIndex]
+        if (variable) {
+          dispatch(attachControlToVariableAction({
+            controlId,
+            variableId: variable.id,
+            push: false,
+          }))
+        }
+        break
+    }
+  }, [dispatch, pullOptions, pushOptions, controlId, attachedVariableId, programId])
+
+  const isUnused = attachedVariable === undefined
+  const isPushing = attachedVariableSourceControl && attachedVariableSourceControl.id === control.id
+
+  let variant = 'unused'
+  let selectLabel = 'Use'
+  if (!isUnused) {
+    variant = isPushing ? 'push' : 'pull'
+    selectLabel = isPushing ? 'Push' : 'Pull'
+  }
+
+  let selectValue = 'u'
+  const selectElements: SelectViewElement[] = [{
+    type: 'option',
+    label: 'Unused',
+    value: 'u',
+  }]
+
+  selectElements.push({
+    type: 'group',
+    label: 'Push to',
+    elements: pushOptions.map((variable, index) => ({
+      type: 'option',
+      value: `s${index}`,
+      label: `Variable ${variable.id}`,
+    })).concat([{
+      type: 'option',
+      value: 'n',
+      label: 'New variable',
+    }]),
+  } as SelectViewGroupElement)
+
+  if (attachedVariable !== undefined && isPushing) {
+    const variableIndex = pushOptions.findIndex(v => v.id === attachedVariable.id)
+    if (variableIndex !== -1) {
+      selectValue = `s${variableIndex}`
+    }
+  }
+
+  if (pullOptions.length > 0) {
+    selectElements.push({
+      type: 'group',
+      label: 'Pull from',
+      elements: pullOptions.map((variable, index) => ({
+        type: 'option',
+        value: `l${index}`,
+        label: `Variable ${variable.id}`,
+      })),
+    })
+    if (attachedVariable !== undefined && !isPushing) {
+      const variableIndex = pullOptions.findIndex(v => v.id === attachedVariable.id)
+      if (variableIndex !== -1) {
+        selectValue = `l${variableIndex}`
+      }
+    }
+  }
+
+  return (
+    <SelectView
+      elements={selectElements}
+      value={selectValue}
+      valueLabel={selectLabel}
+      onChange={onSelectChange}
+      modifiers={['height-6', 'chevron-first'].concat(isUnused ? ['meta'] : ['primary'])}
+    />
+  )
+}
--- src/views/outlet/outlet.tsx
+++ src/views/outlet/outlet.tsx
@@ -1,20 +1,14 @@
 
 import './outlet.scss'
-import SelectView, { SelectViewElement, SelectViewGroupElement } from 'views/select/select'
-import {
-  addVariableFromControlAction,
-  attachControlToVariableAction,
-  detachControlFromVariableAction
-} from 'slices/blueprint'
 import { ControlNode } from 'slices/blueprint/types/control'
 import { ProgramNode } from 'slices/blueprint/types/program'
 import { ReactComponent as OutletPullIcon } from 'icons/outlet-pull.svg'
 import { ReactComponent as OutletPushIcon } from 'icons/outlet-push.svg'
 import { ReactComponent as OutletUnusedIcon } from 'icons/outlet-unused.svg'
 import { getControlVariable, getVariableControl } from 'slices/blueprint/selectors/variable'
-import { getControlVariableOptions } from 'slices/blueprint/selectors/control'
-import { useAppDispatch, useBlueprintSelector } from 'utils/hooks'
-import { MouseEventHandler, useCallback } from 'react'
+import { useAppClassName, useBlueprintSelector } from 'utils/hooks'
+import { MouseEventHandler } from 'react'
+import OutletSelectView from 'views/outlet-select/outlet-select'
 
 export default function OutletView(props: {
   control: ControlNode
@@ -22,134 +16,22 @@ export default function OutletView(props: {
   expanded: boolean
   onIndicatorClick?: MouseEventHandler<HTMLButtonElement>
 }) {
-  const dispatch = useAppDispatch()
   const { control, program } = props
-  const controlId = control.id
-  const programId = program.id
-
-  const { pushOptions, pullOptions } = useBlueprintSelector(state =>
-    getControlVariableOptions(state, props.control.id, props.program.id))
 
   const attachedVariable = useBlueprintSelector(state =>
     getControlVariable(state, props.control.id, props.program.id))
   const attachedVariableSourceControl = useBlueprintSelector(state =>
     attachedVariable ? getVariableControl(state, attachedVariable.id) : undefined)
-  const attachedVariableId = attachedVariable?.id
-
-  const onSelectChange = useCallback((event: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = event.target.value
-    const type = value[0]
-    const choiceIndex = parseInt(value.substring(1))
-    let variable
-    switch (type) {
-      case 'u':
-        if (attachedVariableId !== undefined) {
-          // Detach variable
-          dispatch(detachControlFromVariableAction({
-            controlId,
-            variableId: attachedVariableId,
-          }))
-        }
-        break
-      case 's':
-        // Push to a variable
-        variable = pushOptions[choiceIndex]
-        if (variable) {
-          dispatch(attachControlToVariableAction({
-            controlId,
-            variableId: variable.id,
-            push: true,
-          }))
-        }
-        break
-      case 'n':
-        // Push to a new variable
-        dispatch(addVariableFromControlAction({
-          controlId,
-          programId,
-        }))
-        break
-      case 'l':
-        // Pull from a variable
-        variable = pullOptions[choiceIndex]
-        if (variable) {
-          dispatch(attachControlToVariableAction({
-            controlId,
-            variableId: variable.id,
-            push: false,
-          }))
-        }
-        break
-    }
-  }, [dispatch, pullOptions, pushOptions, controlId, attachedVariableId, programId])
 
   const isUnused = attachedVariable === undefined
   const isPushing = attachedVariableSourceControl && attachedVariableSourceControl.id === control.id
-
-  let variant = 'unused'
-  let selectLabel = 'Use'
-  if (!isUnused) {
-    variant = isPushing ? 'push' : 'pull'
-    selectLabel = isPushing ? 'Push' : 'Pull'
-  }
-
-  let selectValue = 'u'
-  const selectElements: SelectViewElement[] = [{
-    type: 'option',
-    label: 'Unused',
-    value: 'u',
-  }]
-
-  selectElements.push({
-    type: 'group',
-    label: 'Push to',
-    elements: pushOptions.map((variable, index) => ({
-      type: 'option',
-      value: `s${index}`,
-      label: `Variable ${variable.id}`,
-    })).concat([{
-      type: 'option',
-      value: 'n',
-      label: 'New variable',
-    }]),
-  } as SelectViewGroupElement)
-
-  if (attachedVariable !== undefined && isPushing) {
-    const variableIndex = pushOptions.findIndex(v => v.id === attachedVariable.id)
-    if (variableIndex !== -1) {
-      selectValue = `s${variableIndex}`
-    }
-  }
-
-  if (pullOptions.length > 0) {
-    selectElements.push({
-      type: 'group',
-      label: 'Pull from',
-      elements: pullOptions.map((variable, index) => ({
-        type: 'option',
-        value: `l${index}`,
-        label: `Variable ${variable.id}`,
-      })),
-    })
-    if (attachedVariable !== undefined && !isPushing) {
-      const variableIndex = pullOptions.findIndex(v => v.id === attachedVariable.id)
-      if (variableIndex !== -1) {
-        selectValue = `l${variableIndex}`
-      }
-    }
-  }
+  const variant = isUnused ? 'unused' : (isPushing ? 'push' : 'pull')
 
   return (
-    <div className={`outlet outlet--${variant}` + (props.expanded ? ' outlet--expanded' : '')}>
+    <div className={useAppClassName('outlet', [variant].concat(props.expanded ? ['expanded'] : ''))}>
       {props.expanded && (
         <div className="outlet__select">
-          <SelectView
-            elements={selectElements}
-            value={selectValue}
-            valueLabel={selectLabel}
-            onChange={onSelectChange}
-            modifiers={['height-6', 'chevron-first'].concat(isUnused ? ['meta'] : ['primary'])}
-          />
+          <OutletSelectView control={control} program={program} />
         </div>
       )}
       <button
