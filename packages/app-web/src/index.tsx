4368a2fa4bd2532ee9da167dee39e77a42c30402
Fränz Friederes <fraenz@frieder.es>
2022-06-25 14:36:36 +0200

    Upgrade to React 18

--- packages/app-web/src/index.tsx
+++ packages/app-web/src/index.tsx
@@ -2,15 +2,16 @@
 import './styles/index.scss'
 import AppView from './views/app/app'
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { createRoot } from 'react-dom/client'
 import { store } from './store'
 
-ReactDOM.render(
+const rootContainer = document.getElementById('root') as HTMLElement
+const root = createRoot(rootContainer)
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <AppView />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
