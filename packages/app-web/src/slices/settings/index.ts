738e6ad73411580bea298364c257bf0cb06669f0
robigan <35210888+robigan@users.noreply.github.com>
2022-08-30 15:07:55 +0200

    Add control+k to toggleAddModalAction

--- packages/app-web/src/slices/settings/index.ts
+++ packages/app-web/src/slices/settings/index.ts
@@ -27,7 +27,8 @@ const defaultSettingsState: SettingsState = {
     'control+shift+z': redoAction.type,
     'control+y': redoAction.type, // Windows and Linux systems that use Cinnamon as a DE use Ctrl+y for redo. See https://support.apple.com/en-ie/guide/pages/tana7e101d4c/mac, https://support.microsoft.com/en-us/office/undo-redo-or-repeat-an-action-84bdb9bc-4e23-4f06-ba78-f7b893eb2d28, and https://cheatography.com/shakiestnerd/cheat-sheets/linux-mint-cinnamon/
     'escape': [popModalAction.type, endWireAction.type],
-    'meta+k': toggleAddModalAction.type
+    'meta+k': toggleAddModalAction.type,
+    'control+k': toggleAddModalAction.type,
     /* eslint-enable quote-props */
   }
 }
