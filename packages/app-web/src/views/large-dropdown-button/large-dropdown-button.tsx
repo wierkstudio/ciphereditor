5c34fec4c009a1c2c36b04a7f09327915183585b
robigan <35210888+robigan@users.noreply.github.com>
2022-09-05 12:44:18 +0000

    Add large-dropdown-button component

---
+++ packages/app-web/src/views/large-dropdown-button/large-dropdown-button.tsx
@@ -0,0 +1,21 @@
+import { PropsWithChildren } from "react";
+import ButtonView from "../button/button";
+import IconView, { Icon } from "../../views/icon/icon";
+
+interface LargeDropDownButtonPayload extends PropsWithChildren {
+  /**
+   * Icon is optional and can be supplemented via the children prop using IconView
+   */
+  icon?: Icon
+}
+
+export default function LargeDropDownButtonView(props: LargeDropDownButtonPayload) {
+  const { children, icon } = props
+  return (
+    <ButtonView modifiers={["large"]}>
+      <span style={{ fontSize: "1.5em", lineHeight: "2em", display: "flex", alignItems: "center" }}>
+        {children} {icon !== undefined ? <IconView icon={icon} /> : <></> }
+      </span>
+    </ButtonView>
+  )
+}
