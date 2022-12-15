2cf6dfd23e81d0617ec698cf6756d1a596e5e179
Fränz Friederes <fraenz@frieder.es>
2022-12-15 11:47:44 +0100

    Update build pipeline

--- packages/extension-pgp/rollup.config.mjs
+++ packages/extension-pgp/rollup.config.mjs
@@ -3,6 +3,10 @@ import nodeResolve from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+global['__filename'] = __filename;
+
 export default {
   input: './src/index.ts',
   output: {
